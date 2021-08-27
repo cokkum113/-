867cfb34c5b05e1d92c07ea7702d4ab67aa84c9e
cokkum113 <84273370+cokkum113@users.noreply.github.com>
2021-08-27 18:08:43 +0900

    Test클래스에 다시 넣기 오타수정

--- CH6/src/main/java/com/example/chap01_userinfo/UserServiceImpl.java
+++ CH6/src/main/java/com/example/chap01_userinfo/UserServiceImpl.java
@@ -92,53 +92,7 @@ public class UserServiceImpl implements UserService {
     public void setTransactionManager(DataSourceTransactionManager transactionManager) {
     }
 
-    static class MockUserDao implements UserDao {
 
-        private List<User> users;
-        private List<User> updated = new ArrayList<>();
-
-        private MockUserDao(List<User> users) {
-            this.users = users;
-        }
-
-        public List<User> getUpdated() {
-            return this.updated;
-        }
-
-        public List<User> getAll() {
-            return this.users;
-        }
-
-        public void update(User user) {
-            updated.add(user);
-        }
-
-        /*
-        테스트에 사용되지 않는 메소드들
-         */
-
-        @Override
-        public void add(User user) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public User get(String id) {
-            throw new UnsupportedOperationException();
-        }
-
-
-        @Override
-        public void deleteAll() {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public int getCount() {
-            throw new UnsupportedOperationException();
-
-        }
-    }
 
 
 }
--- CH6/src/test/java/com/example/chap01_userinfo/UserServiceTest.java
+++ CH6/src/test/java/com/example/chap01_userinfo/UserServiceTest.java
@@ -178,6 +178,56 @@ public class UserServiceTest {
         //목 오브젝트를 이용한 결과 확인.
     }
 
+    static class MockUserDao implements UserDao {
+
+        private List<User> users;
+        private List<User> updated = new ArrayList<>();
+
+        private MockUserDao(List<User> users) {
+            this.users = users;
+        }
+
+        public List<User> getUpdated() {
+            return this.updated;
+        }
+
+        public List<User> getAll() {
+            return this.users;
+        }
+
+        public void update(User user) {
+            updated.add(user);
+        }
+
+        /*
+        테스트에 사용되지 않는 메소드들
+         */
+
+        @Override
+        public void add(User user) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public User get(String id) {
+            throw new UnsupportedOperationException();
+        }
+
+
+        @Override
+        public void deleteAll() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public int getCount() {
+            throw new UnsupportedOperationException();
+
+        }
+
+
+    }
+
 
 
 
